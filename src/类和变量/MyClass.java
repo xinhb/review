0c40e148259183a8fa944310457dd1687968b6e9
xinhb <2607068641@qq.com>
2019-06-14 22:42:34 +0800

    类

--- src/类和变量/MyClass.java
+++ src/类和变量/MyClass.java
@@ -4,7 +4,7 @@ package 类和变量;
  * Java中变量分三种：成员变量、静态变量、局部变量
  * 1、成员变量的修饰符有public、private、protected、default（不写）
  * 2、修饰局部变量不能用上面的
- * 3、类的修饰符：public、abstract、final、default
+ * 3、类的修饰符：public、abstract、final、defaul
  *
  * 补充：
  *  1、一个Java文件可以定义多个类，但只能有一个了类是public且与文件名相同。编译时会给每个类生成一个对应的.class文件
